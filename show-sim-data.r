1259b714933e839f21d96276c2f7b96e9ff2d13b
Yuri <yuri@rawbw.com>
2019-01-11 00:01:47 -0800

    Minor R-script update

--- show-sim-data.r
+++ show-sim-data.r
@@ -5,7 +5,7 @@ fn <- buckets
 for(vv in buckets[1]) {
   r <- 1
   for (v in vv) {
-    fn[r,2] <- fn[r,2]/(4*3.141592*fn[r,1]*fn[r,1])*10 # 10 is for scale
+    fn[r,2] <- fn[r,2]/(4*pi*fn[r,1]^2)*2900000 # 10 is for scale
     r <- r+1
   }
 }
